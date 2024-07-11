176f3701fba686bef5e4c0b18e00a8a25280bc25
124476 <124476@bk.ru>
2024-07-11 23:04:47 +0300

    Uplode work with video

---
+++ ArhivVideos/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using ArhivVideos.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArhivVideos
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyFrame.Navigate(new PageMain());
+        }
+
+        private void MyFrame_ContentRendered(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ArhivVideos/Pages/PageMain.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArhivVideos.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageMain.xaml
+    /// </summary>
+    public partial class PageMain : Page
+    {
+        public PageMain()
+        {
+            InitializeComponent();
+        }
+
+        private void GetVideo_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog() { Filter = "*.mov;*.mp4; | *.mov;*.mp4;" };
+            if (dialog.ShowDialog().GetValueOrDefault())
+            {
+                VideoControl.Source = new Uri(dialog.FileName);
+                VideoControl.Play();
+            }
+        }
+
+        private void RunVideo_Click(object sender, RoutedEventArgs e)
+        {
+            VideoControl.Play();
+        }
+
+        private void StopVideo_Click(object sender, RoutedEventArgs e)
+        {
+            VideoControl.Pause();
+        }
+    }
+}
